07be217c1d05a90a3c57e3fddad8cdb564f7429e
Chinmaya97 <chinmaya.nayak.2697@gmail.com>
2025-12-28 19:47:15 +0530

    feat: added user & video model

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHandler = (fn) => (req, res, next) => {
+  try {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export { asyncHandler };
